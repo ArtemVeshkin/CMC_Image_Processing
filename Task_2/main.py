6a0bde9d373d276da1cb475cbd5e929d81b1854a
Artem <46985381+ArtemVeshkin@users.noreply.github.com>
2021-01-28 15:44:48 +0300

    Update main.py

--- Task_2/main.py
+++ Task_2/main.py
@@ -55,6 +55,38 @@ def gen_noisy_image(image, sigma):
     return make_image(clean)
 
 
+def extend(image, size):
+    pixels = np.array(image)[:, :, :3]
+    width, height = image.size
+    new_image_pixels = np.zeros((height + 2 * size, width + 2 * size, 3), dtype=np.uint8)
+
+    for i in range(new_image_pixels.shape[0]):
+        for j in range(new_image_pixels.shape[1]):
+            if (size <= i < height + size) and (size <= j < width + size):
+                new_image_pixels[i, j] = pixels[i - size, j - size]
+            elif i < size:
+                if j < size:
+                    new_image_pixels[i, j] = pixels[0, 0]
+                elif j < size + width:
+                    new_image_pixels[i, j] = pixels[0, j - size]
+                else:
+                    new_image_pixels[i, j] = pixels[0, width - 1]
+            elif size <= i < height + size:
+                if j < size:
+                    new_image_pixels[i, j] = pixels[i - size, 0]
+                else:
+                    new_image_pixels[i, j] = pixels[i - size, width - 1]
+            else:
+                if j < size:
+                    new_image_pixels[i, j] = pixels[height - 1, 0]
+                elif j < size + width:
+                    new_image_pixels[i, j] = pixels[height - 1, j - size]
+                else:
+                    new_image_pixels[i, j] = pixels[height - 1, width - 1]
+
+    return Image.fromarray(new_image_pixels)
+
+
 # mse (input_file_1) (input_file_2)
 def mse(params):
     image1 = np.array(Image.open(params[0]), dtype="float64")
@@ -85,9 +117,39 @@ def median(params):
     return make_image(clean)
 
 
+def gauss(x, sigma):
+    return 1 / (2 * np.pi * sigma ** 2) * np.exp(-x ** 2 / (2 * sigma ** 2))
+
+
+def generate_mask(mask_size, sigma_d, sigma_r):
+    mask = np.zeros((mask_size * 2 + 1, mask_size * 2 + 1))
+    for i in range(-mask_size, mask_size + 1):
+        for j in range(-mask_size, mask_size + 1):
+            mask[mask_size + i, mask_size + j] = gauss(, sigma_d) * gauss(, sigma_r)
+    return mask
+
+
 # bilateral (sigma_d) (sigma_r) (input_file) (output_file)
 def bilateral(params):
-    pass
+    sigma_d = float(params[0])
+    sigma_r = float(params[1])
+
+    mask_size = 2
+
+    # Using 6*sigma x 6*sigma Gaussian mask
+    mask = generate_mask(ms, lambda x, y: math.exp(-(x ** 2 + y ** 2) / (2 * (sigma ** 2))))
+    mask /= mask.sum()
+
+    pixels = np.array(extend(["dup", str(ms)], input_file))
+    new_pixels = np.zeros((pixels.shape[0] - ms * 2, pixels.shape[1] - ms * 2, 3))
+
+    for i in range(new_pixels.shape[0]):
+        for j in range(new_pixels.shape[1]):
+            # Convolution
+            for c in range(3):
+                new_pixels[i, j, c] = pixels[i:i + 2 * ms + 1, j:j + 2 * ms + 1, c].dot(mask).diagonal().sum()
+
+    return Image.fromarray(normalize(new_pixels))
 
 
 # query (noise_level)
